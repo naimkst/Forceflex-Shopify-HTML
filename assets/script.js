c1597f6d978d46d3d89568d786686d156cd581a4
shahin-shohan <56084608+shahin-shohan@users.noreply.github.com>
2024-08-04 00:58:56 +0600

    scremove

--- assets/script.js
+++ assets/script.js
@@ -114,77 +114,6 @@
         $('.open-btn').removeClass('x-close');
     });
 
-    // toggle3
-    $('#toggle3').on("click", function () {
-        $('#open3').slideToggle();
-        $('.caupon-wrap.s3').toggleClass('coupon-3')
-    })
-
-
-    // HERO SLIDER
-    var menu = [];
-    jQuery('.swiper-slide').each(function (index) {
-        menu.push(jQuery(this).find('.slide-inner').attr("data-text"));
-    });
-    var interleaveOffset = 0.5;
-    var swiperOptions = {
-        loop: true,
-        speed: 1000,
-        parallax: true,
-        autoplay: {
-            delay: 6500,
-            disableOnInteraction: false,
-        },
-        watchSlidesProgress: true,
-        pagination: {
-            el: '.swiper-pagination',
-            clickable: true,
-        },
-
-        navigation: {
-            nextEl: '.swiper-button-next',
-            prevEl: '.swiper-button-prev',
-        },
-
-        on: {
-            progress: function () {
-                var swiper = this;
-                for (var i = 0; i < swiper.slides.length; i++) {
-                    var slideProgress = swiper.slides[i].progress;
-                    var innerOffset = swiper.width * interleaveOffset;
-                    var innerTranslate = slideProgress * innerOffset;
-                    swiper.slides[i].querySelector(".slide-inner").style.transform =
-                        "translate3d(" + innerTranslate + "px, 0, 0)";
-                }
-            },
-
-            touchStart: function () {
-                var swiper = this;
-                for (var i = 0; i < swiper.slides.length; i++) {
-                    swiper.slides[i].style.transition = "";
-                }
-            },
-
-            setTransition: function (speed) {
-                var swiper = this;
-                for (var i = 0; i < swiper.slides.length; i++) {
-                    swiper.slides[i].style.transition = speed + "ms";
-                    swiper.slides[i].querySelector(".slide-inner").style.transition =
-                        speed + "ms";
-                }
-            }
-        }
-    };
-
-    var swiper = new Swiper(".swiper-container", swiperOptions);
-
-    // DATA BACKGROUND IMAGE
-    var sliderBgSetting = $(".slide-bg-image");
-    sliderBgSetting.each(function (indx) {
-        if ($(this).attr("data-background")) {
-            $(this).css("background-image", "url(" + $(this).data("background") + ")");
-        }
-    });
 
     //Setting hero slider
     function heroSlider() {
@@ -310,49 +239,6 @@
     });
 
 
-    /*------------------------------------------
-        = Featured SLIDER
-    -------------------------------------------*/
-    if ($(".category-slider-active").length) {
-        $(".category-slider-active").owlCarousel({
-            autoplay: true,
-            smartSpeed: 300,
-            margin: 0,
-            loop: true,
-            autoplayHoverPause: true,
-            dots: false,
-            navText: ['<i class="icon-angale-right"></i>', '<i class="icon-angale-right2"></i>'],
-            nav: false,
-            responsive: {
-                0: {
-                    items: 1,
-                },
-
-                350: {
-                    items: 2,
-                },
-                500: {
-                    items: 3,
-                },
-
-                768: {
-                    items: 4,
-                },
-                992: {
-                    items: 5,
-                },
-
-                1200: {
-                    items: 6
-                },
-
-                1400: {
-                    items: 6
-                },
-
-            }
-        });
-    }
     /*------------------------------------------
        product slider
     -------------------------------------------*/
@@ -679,48 +565,5 @@
             smallNavFunctionality();
         }, 200));
     });
-
-
-
-    /*------------------------------------------
-        = SHOP DETAILS PAGE PRODUCT SLIDER
-    -------------------------------------------*/
-    if ($(".shop-single-slider").length) {
-        $('.slider-for').slick({
-            slidesToShow: 1,
-            slidesToScroll: 1,
-            arrows: false,
-            fade: true,
-            asNavFor: '.slider-nav',
-        });
-        $('.slider-nav').slick({
-            slidesToShow: 10,
-            slidesToScroll: 1,
-            asNavFor: '.slider-for',
-            vertical: true,
-            verticalSwiping: true,
-            focusOnSelect: true,
-            arrows: false,
-
-            responsive: [
-                {
-                    breakpoint: 500,
-                    settings: {
-                        slidesToShow: 5,
-                        infinite: true
-                    }
-                },
-                {
-                    breakpoint: 400,
-                    settings: {
-                        slidesToShow: 5
-                    }
-                }
-            ]
-
-        });
-    }
-
-
 
 })(window.jQuery);
